2dff7fa24ede2f1e82887fc45aaec93b08d421c0
Yunzhi Huang <yunzhi_huang@idgcapital.com>
2023-01-31 11:04:13 +0800

    修复接口失败问题

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -3,7 +3,34 @@ import cityList from './cityList';
 
 const apikey = '58dbdb3c33524f6ab7c906ba18354d1f';
 const baiduKey = '8XXHbc9FpQC6nnH780fbzhgbG7vOtust';
-const Letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const Letters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
 const provinceData = [];
 const cityData = {};
 
@@ -42,18 +69,35 @@ for (let i in cityList) {
 
 for (let province of provinceData) {
   const s = new Set(province.values);
-  province.values = Array.from(s).map(x => x);
+  province.values = Array.from(s).map((x) => x);
 }
 
-const getCity = () => Vue.http.jsonp(`http://api.map.baidu.com/location/ip?ak=${baiduKey}&coor=bd09ll`)
-  .then(response => response.body.content.address_detail.city.split('市')[0]);
+const getCity = () =>
+  Vue.http
+    .jsonp(`https://api.map.baidu.com/location/ip?ak=${baiduKey}&coor=bd09ll`)
+    .then(
+      (response) => response.body.content.address_detail.city.split('市')[0]
+    ).catch(() => {
+      console.error('根据IP获取城市信息失败');
+      return '北京';
+    });
 
-const queryWeather = (resolve, city) => Vue.http.get(`https://free-api.heweather.com/s6/weather?key=${apikey}&location=${city}`)
-  .then(res => resolve(res.body['HeWeather6'][0]));
+const queryWeather = (resolve, city) =>
+  Vue.http
+    .get(
+      `https://free-api.heweather.com/s6/weather?key=${apikey}&location=${city}`
+    )
+    .then((res) => resolve(res.body['HeWeather6'][0]));
 
-export const getWeather = args => new Promise((resolve, reject) => args
-  ? queryWeather(resolve, args) : getCity().then(city => queryWeather(resolve, city)));
+export const getWeather = (args) =>
+  new Promise((resolve, reject) =>
+    args
+      ? queryWeather(resolve, args)
+      : getCity().then((city) => queryWeather(resolve, city))
+  );
 
-export const loadProvince = args => new Promise((resolve, reject) => resolve({errno: 0, data: provinceData}));
+export const loadProvince = (args) =>
+  new Promise((resolve, reject) => resolve({ errno: 0, data: provinceData }));
 
-export const loadCity = args => new Promise((resolve, reject) => resolve({errno: 0, data: cityData}));
+export const loadCity = (args) =>
+  new Promise((resolve, reject) => resolve({ errno: 0, data: cityData }));
